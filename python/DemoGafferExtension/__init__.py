14a3443b3e8371b57974e705f84b1c1dd799023a
Andrew Kaufman <andrewk@image-engine.com>
2019-01-03 11:02:19 -0800

    Added missing imports

--- python/DemoGafferExtension/__init__.py
+++ python/DemoGafferExtension/__init__.py
@@ -1,3 +1,6 @@
+__import__( "Gaffer" )
+__import__( "GafferScene" )
+
 from _DemoGafferExtension import *
 
 __import__( "IECore" ).loadConfig( "GAFFER_STARTUP_PATHS", subdirectory = "DemoGafferExtension" )
