350b57bdcd854ba90c1685b207d00037ef77cb83
Andrew Kaufman <andrewk@image-engine.com>
2020-11-12 14:03:53 -0800

    GUI : Fix missing node metadata.

--- startup/gui/menu.py
+++ startup/gui/menu.py
@@ -1,6 +1,7 @@
 import GafferUI
 
 import DemoGafferExtension
+import DemoGafferExtensionUI
 
 nodeMenu = GafferUI.NodeMenu.acquire( application )
 nodeMenu.append( "/DemoGafferExtension/DemoSceneProcessor", DemoGafferExtension.DemoSceneProcessor, searchText = "Demo SceneProcessor" )
